a353c3cbc9b1f09bd189668aa19968c2a1b7505d
Alexander Schieving <alexanderschieving@gmail.com>
2017-07-05 13:29:16 +0200

    One-to-One Association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :events, dependent: :destroy
+  has_many :events, dependent: :destroy
+
+  has_one :profile
+
+  def has_profile?
+    profile.present? && profile.persisted?
+  end
+
+  def full_name
+    profile.full_name
+  end
+
 
 end
